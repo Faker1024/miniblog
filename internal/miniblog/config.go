63b9314a842f37d56be64d3ac58546697f2b03aa
jack <3361935899@qq.com>
2024-01-07 23:31:35 +0800

    读取配置部分

---
+++ internal/miniblog/config.go
@@ -0,0 +1,69 @@
+// MIT License
+//
+// Copyright (c) 2024 jack 3361935899@qq.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package miniblog
+
+import (
+	"fmt"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"strings"
+)
+
+var cfgFile string
+
+// 指定配置文件名
+const defaultConfigName = "miniblog"
+
+func initConfig() {
+	if cfgFile != "" {
+		//	当配置文件位置被传入，直接使用传入的配置文件
+		viper.AddConfigPath(cfgFile)
+	} else {
+		//	如果未传入配置文件位置，则尝试读取Home文件夹下的配置文件
+		home, err := homedir.Dir()
+		// 当err不为空，输出错误并退出
+		cobra.CheckErr(err)
+		//将home加入配置文件搜索路径
+		viper.AddConfigPath(home)
+		//将当前目录加入配置文件搜索路径
+		viper.AddConfigPath(".")
+		//将配置文件格式定义为yaml文件
+		viper.SetConfigType("yaml")
+		//配置文件名
+		viper.SetConfigName(defaultConfigName)
+	}
+	//读取匹配的环境变量
+	viper.AutomaticEnv()
+	//设置读取的环境变量前缀，如果为小写则自动转化为大写
+	viper.SetEnvPrefix("MINIBLOG")
+	//将viper.Get(key)字符串中‘.’和‘-’替换为‘_’
+	replacer := strings.NewReplacer(".", "-")
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+	//打印viper当前使用的配置文件，方便后续的Debug.
+	fmt.Println(os.Stdout, "Using config file", viper.ConfigFileUsed())
+}
--- internal/miniblog/miniblog.go
+++ internal/miniblog/miniblog.go
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2024 jack 3361935899@qq.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package miniblog
 
 import (
@@ -23,8 +45,13 @@ func NewMiniBlogCommand() *cobra.Command {
 			return nil
 		},
 	}
+	//以下设置使得initConfig函数在每个命令运行时都会被调用已读取配置
+	cobra.OnInitialize(initConfig)
+	// Cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default os $HOME/.yaml)")
+	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
+	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	return cmd
-
 }
 
 // 实际业务代码入口
